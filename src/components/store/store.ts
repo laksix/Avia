0365ae28de6d8bc047918010db6d5c7ed4eaaada
laksix <147379934+laksix@users.noreply.github.com>
2024-03-11 01:02:39 +0700

    Update store.ts

--- src/components/store/store.ts
+++ src/components/store/store.ts
@@ -1,1 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';import { combineReducers } from 'redux';import filterReducer from './reducers/FilterSlice';import AsideFilter from './reducers/AsideFilter';import Tickets from './reducers/TIcketsSlice';// import TicketsFilter from "./reducers/TicketsFIlter";const rootReducer = combineReducers({  filterReducer,  AsideFilter,  Tickets,});export const setupStore = () => {  return configureStore({    reducer: rootReducer,  });};export type RootState = ReturnType<typeof rootReducer>;export type AppStore = ReturnType<typeof setupStore>;export type AppDispatch = AppStore['dispatch'];
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import filterReducer from './reducers/FilterSlice';
+import AsideFilter from './reducers/AsideFilter';
+import Tickets from './reducers/TIcketsSlice';
+// import TicketsFilter from "./reducers/TicketsFIlter";
+const rootReducer = combineReducers({
+  filterReducer,
+  AsideFilter,
+  Tickets,
+});
+
+export const setupStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+  });
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
